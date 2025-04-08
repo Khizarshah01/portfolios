9672f5d89f613c5da06502b99c6f7b4056563023
Khizarshah01 <khizar.shah010@gmail.com>
2025-04-08 18:29:04 +0530

    removed the unwanted data

--- src/data/socialsData.jsx
+++ src/data/socialsData.jsx
@@ -2,13 +2,10 @@ export const socialsData = {
     github: 'https://github.com/Khizarshah01',
     linkedIn: 'https://www.linkedin.com/in/khizarshah01/',
     instagram: 'https://www.instagram.com/khizar_shah01/',
-    codepen: 'https://codepen.io/',
-    facebook: 'https://www.facebook.com/',
     twitter: 'https://x.com/Khizarshah_01',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
     stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
     youtube: 'https://youtube.com/'
 }
